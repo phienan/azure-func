5dc1a9e73bbd612e493359be1611241a5e4d008a
P.A <P.A>
2020-02-23 22:53:24 -0600

    Updated version 2.0

--- HttpHello/__init__.py
+++ HttpHello/__init__.py
@@ -6,4 +6,4 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    return func.HttpResponse(f"Hello World!")
+    return func.HttpResponse(f"Hello Python! You are seeing the new version here")
